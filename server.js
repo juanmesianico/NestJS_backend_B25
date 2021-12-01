da13eb8065804cd2fae6189a437246d589823ae5
Juan Mesa <upsnoexisten@gmail.com>
2021-11-30 23:07:55 -0500

    Bug fixed in server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 
-const app = express(__asyncDelegator);
+const app = express();
 
 app.use(express.static(__dirname + 'dist/frontend'));
 
